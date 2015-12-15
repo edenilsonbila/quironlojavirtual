3256736a69dd4b8f523fa341e9a93960df165740
edenilsonbila <edenilsonbila@yahoo.com>
2015-12-15 21:12:15 -0200

    BootStrap

--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Quiron.LojaVirtual.Web
@@ -12,6 +8,12 @@ namespace Quiron.LojaVirtual.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name:null,
+                url:"Pagina{pagina}",
+                defaults:new {controller = "Vitrine", action = "ListaProdutos"}
+                );
 
             routes.MapRoute(
                 name: "Default",

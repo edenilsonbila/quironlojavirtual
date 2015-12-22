2a92173dece1c63e587ca6e21d1b5917c4ca2e5f
edenilsonbila <edenilsonbila@yahoo.com>
2015-12-22 13:44:36 -0200

    Aula 25 - Envio E-mail Pedido

---
+++ Quiron.LojaVirtual.Dominio/Entidades/EmailConfiguracoes.cs
@@ -0,0 +1,14 @@
+﻿namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class EmailConfiguracoes
+    {
+        public bool UsarSsl = false;
+        public string ServidorSmtp = "smtp.quiron.com.br";
+        public int ServidorPorta = 587;
+        public string Usuario = "quiron";
+        public bool EscreverArquivo = false;
+        public string PastaArquivo = "@c:\enviaemail";
+        public string De = "Eu";
+        public string Para = "Voce";
+    }
+}

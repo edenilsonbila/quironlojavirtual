0ba88471f6c903ac49c0fb8c9e4c2bb87c9329a5
edenilsonbila <edenilsonbila@yahoo.com>
2015-12-23 20:03:57 -0200

    Aula 33 - Validações

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -9,11 +9,15 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
         [HiddenInput(DisplayValue = false)]
         public int ProdutoID { get; set; }
 
+        [Required(ErrorMessage = "Nome é Obrigatório")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Descrição é Obrigatório")]
         [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "Digite um valor")]
+        [Range(0.01,double.MaxValue, ErrorMessage = "Valor inválido")]
         public decimal Preco { get; set; }
 
         public string Categoria { get; set; }

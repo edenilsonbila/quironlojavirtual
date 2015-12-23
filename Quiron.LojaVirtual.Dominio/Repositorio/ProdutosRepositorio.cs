90ee8e8a787e0defa68d73f6ce1d64b38bf6e770
edenilsonbila <edenilsonbila@yahoo.com>
2015-12-23 19:18:08 -0200

    Aula 32 - Alterar Salvar Produto

--- Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -17,5 +17,32 @@ namespace Quiron.LojaVirtual.Dominio.Repositorio
             get { return _context.Produtos; }
         } 
 
+        //Salvar Produto - Alterar Produto
+
+        public void Salvar(Produto produto)
+        {
+            if (produto.ProdutoID == 0)
+            {
+                //Salva
+                _context.Produtos.Add(produto);
+            }
+            else
+            {
+                //Altera
+                Produto prod = _context.Produtos.Find(produto.ProdutoID);
+
+                if (prod != null)
+                {
+                    prod.Nome = produto.Nome;
+                    prod.Descricao = produto.Descricao;
+                    prod.Preco = produto.Preco;
+                    prod.Categoria = produto.Categoria;
+
+                }
+            
+            }
+
+            _context.SaveChanges();
+        }
     }
 }

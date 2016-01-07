1fc48a687eec10e294ba11fff8787ab9340f45b1
edenilsonbila <edenilsonbila@yahoo.com>
2016-01-07 16:11:08 -0200

    Aula 65 - Querys

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutoModeloRepositorio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades.Vitrine;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutoModeloRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        //public List<ProdutoVitrine> ObteProduto()
+        //{
+        //    var produtos = _context.ProdutoVitrine
+        //        .OrderBy(r => Guid.NewGuid())
+        //        .Take(20).ToList();
+
+        //    return produtos;
+        //}
+
+        //public List<ProdutoVitrine> ObteProdutoPorGenero(string genero)
+        //{
+        //    var produtos = _context.ProdutoVitrine
+        //        .Where(p => p.GeneroCodigo == genero)
+        //        .OrderBy(r => Guid.NewGuid())
+        //        .Take(20).ToList();
+
+        //    return produtos;
+        //}
+
+        public List<ProdutoVitrine> ObterProdutoVitrine(string categoria = null, string genero = null,
+            string grupo = null, string modalidade = null,string subgrupo = null,  string linha = null,
+            string marca = null)
+        {
+            var query = from p in _context.ProdutoVitrine select p;
+
+            if (!string.IsNullOrEmpty(categoria))  
+                query = query.Where(p => p.CategoriaCodigo == categoria);
+
+            if (!string.IsNullOrEmpty(genero))
+                query = query.Where(p => p.GeneroCodigo == genero);
+
+            if (!string.IsNullOrEmpty(grupo))
+                query = query.Where(p => p.GrupoCodigo == grupo);
+
+            if (!string.IsNullOrEmpty(subgrupo))
+                query = query.Where(p => p.SubGrupoCodigo == subgrupo);
+
+            if (!string.IsNullOrEmpty(linha))
+                query = query.Where(p => p.LinhaCodigo == linha);
+
+            if (!string.IsNullOrEmpty(marca))
+                query = query.Where(p => p.MarcaCodigo == marca);
+
+            if (!string.IsNullOrEmpty(modalidade))
+                query = query.Where(p => p.ModalidadeCodigo == modalidade);
+
+            query = query.OrderBy(p => Guid.NewGuid());
+            query = query.Take(40);
+
+            return query.ToList();
+        } 
+
+    }
+}
--- Quiron.LojaVirtual.Web.V2/Controllers/NavController.cs
+++ Quiron.LojaVirtual.Web.V2/Controllers/NavController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using Quiron.LojaVirtual.Dominio.Repositorio;
 using Quiron.LojaVirtual.Web.V2.Models;
 
 namespace Quiron.LojaVirtual.Web.V2.Controllers
@@ -15,12 +17,30 @@ namespace Quiron.LojaVirtual.Web.V2.Controllers
             return View();
         }
 
+
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Server, VaryByParam = "none")]
+        public JsonResult TesteMetodoVitrine()
+        {
+            ProdutoModeloRepositorio repositorio = new ProdutoModeloRepositorio();
+
+
+            var produtos = repositorio.ObterProdutoVitrine(categoria: "0083", marca: "1106");
+
+
+            return Json(produtos, JsonRequestBehavior.AllowGet);
+        }
+
+
         [Route("nav/{categoria}/{id}/{marca}")]
         public ActionResult ObterProdutosPorMarcas(string categoria, string id, string marca)
         {
-            var model = new ProdutosViewModel {Produtos = null};
+            ProdutoModeloRepositorio repositorio = new ProdutoModeloRepositorio();
 
-            return View("Index", model);
+         //   repositorio.ObterProdutoVitrine(linha: id, );
+
+          //  var model = new ProdutosViewModel {Produtos = null};
+
+            return View("Index");
         }
 
 
